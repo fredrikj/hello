67c56e05c64465f7a44f9677748bff4de61e717b
Fredrik Johansson <fredrik.johansson@tobii.com>
2022-02-09 12:57:32 +0100

    Exit with code 1 if argument "error"

--- Hello.c
+++ Hello.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #ifdef _WIN32
 #include <Windows.h>
 #else
 #include <unistd.h>
 #endif
 
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc>1 && strcmp(argv[1], "error") == 0) {
+    fprintf(stderr, "I failed\n");
+    exit(1);
+  }
   int i;
   for (i=1; i<=10; i++) {
     sleep(1);
